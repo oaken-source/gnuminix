6db9588770d5794ab3dc7fe81162ced07fd995ec
Andreas Grapentin <andreas@grapentin.org>
2013-10-23 10:07:31 +0200

    added external disk to vagrantfile, isolated vm environment in subdirectory

--- prepare-host.sh
+++ prepare-host.sh
@@ -13,4 +13,14 @@ if [ ! -f LFS-BOOK-7.4.pdf ]; then
   wget -q http://www.linuxfromscratch.org/lfs/downloads/stable/LFS-BOOK-7.4.pdf
 fi
 
-echo "all done."
+if ! vagrant --help > /dev/null; then
+  echo " [!] missing vagrant. install vagrant and continue"
+  exit 1
+fi
+
+if [ -z "$(vagrant box list | grep precise32)" ]; then
+  echo " [*] importing precise32 box for vagrant"
+  vagrant box add precise32 http://files.vagrantup.com/precise32.box > /dev/null
+fi
+
+echo "done."
