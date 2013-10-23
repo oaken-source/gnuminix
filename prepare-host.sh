9d9cce07ce94f36b76c51314206fbc518bb5af38
Andreas Grapentin <andreas@grapentin.org>
2013-10-23 07:00:22 +0200

    beginning infrastructure

---
+++ prepare-host.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+
+if [ -n "${DEBUG:-}" ]; then
+  set -x
+fi
+
+# fetch LFS book
+if [ ! -f LFS-BOOK-7.4.pdf ]; then
+  echo " [*] fetching LFS-BOOK-7.4.pdf"
+  wget -q http://www.linuxfromscratch.org/lfs/downloads/stable/LFS-BOOK-7.4.pdf
+fi
+
+echo "all done."
