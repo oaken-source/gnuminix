ddb598860341df0a0e1c2fe88b3993f9c9f5cecc
Andreas Grapentin <andreas@grapentin.org>
2013-10-23 12:53:49 +0200

    designed license header

--- prepare-host.sh
+++ prepare-host.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+ ##############################################################################
+ #                               GNU / Minix                                  #
+ #                                                                            #
+ #    Copyright (C) 2013  Andreas Grapentin                                   #
+ #                                                                            #
+ #    This program is free software: you can redistribute it and/or modify    #
+ #    it under the terms of the GNU General Public License as published by    #
+ #    the Free Software Foundation, either version 3 of the License, or       #
+ #    (at your option) any later version.                                     #
+ #                                                                            #
+ #    This program is distributed in the hope that it will be useful,         #
+ #    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+ #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+ #    GNU General Public License for more details.                            #
+ #                                                                            #
+ #    You should have received a copy of the GNU General Public License       #
+ #    along with this program.  If not, see <http://www.gnu.org/licenses/>.   #
+ ############################################################################## 
+
+
 set -e
 set -u
 
@@ -13,6 +33,7 @@ if [ ! -f LFS-BOOK-7.4.pdf ]; then
   wget -q http://www.linuxfromscratch.org/lfs/downloads/stable/LFS-BOOK-7.4.pdf
 fi
 
+# check for vagrant installation
 if ! vagrant --help > /dev/null; then
   echo " [!] missing vagrant. install vagrant and continue"
   exit 1
