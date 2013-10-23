2de753750dfd82e33c7b230385c0287f8d1b8aa7
Andreas Grapentin <andreas@grapentin.org>
2013-10-23 23:06:39 +0200

    preparing partitions, fetching guest package tarballs and patches

--- environment/bootstrap.sh
+++ environment/bootstrap.sh
@@ -33,4 +33,36 @@ if [ -n "${DEBUG:-}" ]; then
   set -x
 fi
 
+# installing required packages
+echo " [*] installing and configuring required host packages"
+apt-get update -qq
+apt-get install -yqq vim build-essential bash binutils bison bzip2 coreutils diffutils findutils gawk gcc libc6 grep gzip m4 make patch perl sed tar texinfo xzip  &> /dev/null
+ln -fs /bin/bash /bin/sh
 
+echo " [*] preparing and mounting partitions"
+mkdir -p /mnt/gnu
+if [ -z "$(cat /etc/mtab | grep /mnt/gnu)" ]; then
+  parted -s -- /dev/sdb mklabel gpt
+  parted -s -- /dev/sdb mkpart primary ext2 0 32mb &> /dev/null
+  parted -s -- /dev/sdb mkpart primary linux-swap 32mb 1056mb
+  parted -s -- /dev/sdb mkpart primary ext3 1056mb -1
+
+  mkfs.ext2 -q /dev/sdb1
+  mkfs.ext3 -q /dev/sdb3
+  mkswap /dev/sdb2 > /dev/null
+
+  mount /dev/sdb3 /mnt/gnu 
+  mkdir -p /mnt/gnu/boot
+  mount /dev/sdb1 /mnt/gnu/boot
+  swapon /dev/sdb2
+fi
+
+echo " [*] fetching guest packages"
+mkdir -p /mnt/gnu/sources
+chmod a+wt /mnt/gnu/sources
+wget -q -i all-packages -P /mnt/gnu/sources
+pushd /mnt/gnu/sources
+md5sums -c all-packages.MD5
+popd
+
+echo "done."
