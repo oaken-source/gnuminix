9f193eb7ec453cd11b5461ef14d1f17fdba96a5c
Andreas Grapentin <andreas@grapentin.org>
2013-10-27 19:51:02 +0100

    updated gcc build

--- environment/scripts/5.5.gcc-4.8.1-pass1.sh
+++ environment/scripts/5.5.gcc-4.8.1-pass1.sh
@@ -39,18 +39,18 @@ tar -xf gcc-4.8.1.tar.bz2
 cd gcc-4.8.1
 
 tar -Jxf ../mpfr-3.1.2.tar.xz
-mv -v mpfr-3.1.2 mpfr
+mv mpfr-3.1.2 mpfr
 tar -Jxf ../gmp-5.1.2.tar.xz
-mv -v gmp-5.1.2 gmp
+mv gmp-5.1.2 gmp
 tar -zxf ../mpc-1.0.1.tar.gz
-mv -v mpc-1.0.1 mpc
+mv mpc-1.0.1 mpc
 
 patch -s -i $ROOT/patches/config.sub.patch
 
 for file in \
  $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
 do
-  cp -uv $file{,.orig}
+  cp -u $file{,.orig}
   sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
       -e 's@/usr@/tools@g' $file.orig > $file
   echo '
@@ -67,9 +67,9 @@ mkdir -p ../gcc-build
 cd ../gcc-build
 
 ../gcc-4.8.1/configure                               \
-    --target=$LFS_TGT                                \
+    --target=$GNU_TGT                                \
     --prefix=/tools                                  \
-    --with-sysroot=$LFS                              \
+    --with-sysroot=$GNU                              \
     --with-newlib                                    \
     --without-headers                                \
     --with-local-prefix=/tools                       \
@@ -98,10 +98,10 @@ make &> /mnt/gnu/logs/5.5.build.log \
 make install &> /mnt/gnu/logs/5.5.install.log \
     || (echo " [!] install failed. see /mnt/gnu/logs/5.5.install.log" && exit 1)
 
-ln -s libgcc.a `$GN_TGT-gcc -print-libgcc-file-name | sed 's/libgcc/&_eh/'`
+ln -s libgcc.a `$GNU_TGT-gcc -print-libgcc-file-name | sed 's/libgcc/&_eh/'`
 
 cd ..
 
-rm -rf binutils-2.23.2 binutils-build
+rm -rf gcc-4.8.1 gcc-build
 popd &> /dev/null
 
